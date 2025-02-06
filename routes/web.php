055de70cd9d577a71d2d5a0afca5a6e98f8816cf
Giovanni Barroso <giovanni.baralv@gmail.com>
2025-02-06 10:24:02 +0100

    Added controllers and first view

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,25 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\OrderController;
 
+// Página de inicio
 Route::get('/', function () {
     return view('welcome');
+});
+
+// Agrupar rutas CRUD en un middleware (opcional)
+Route::middleware(['web'])->group(function () {
+    Route::resource('users', UserController::class);
+    Route::resource('products', ProductController::class);
+    Route::resource('categories', CategoryController::class);
+    Route::resource('orders', OrderController::class);
+});
+
+// Ruta de fallback (en caso de error 404)
+Route::fallback(function () {
+    return view('errors.404'); // Asegúrate de tener `resources/views/errors/404.blade.php`
 });
