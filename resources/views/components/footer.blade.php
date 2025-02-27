9f614b2ee5c3523f0df5784a9085d5e21418770a
IvanPerezRosa <ivancds02@gmail.com>
2025-02-27 12:42:42 +0100

    Footer seems to be finished

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,14 +1,14 @@
-<footer class="footer bg-dark text-white py-4 mt-5">
+<footer class="footer text-white py-4 mt-5" style="background-color: #E85C0D;">
     <div class="container">
         <div class="row">
-            <!-- Sección de navegación -->
+            <!-- Sección de navegación (Menú) -->
             <div class="col-md-4">
                 <h5 class="text-uppercase">Menú</h5>
                 <ul class="list-unstyled">
-                    <li><a href="#" class="text-white text-decoration-none">Inicio</a></li>
-                    <li><a href="#" class="text-white text-decoration-none">Conócenos</a></li>
-                    <li><a href="#" class="text-white text-decoration-none">Productos</a></li>
-                    <li><a href="#" class="text-white text-decoration-none">Contáctanos</a></li>
+                    <li><a href="#" class="text-white text-decoration-none">Home</a></li>
+                    <li><a href="/shop" class="text-white text-decoration-none">Products</a></li>
+                    <li><a href="#" class="text-white text-decoration-none">About Us</a></li>
+                    <li><a href="#" class="text-white text-decoration-none">Contact Us</a></li>
                 </ul>
             </div>
 
@@ -18,6 +18,18 @@
                 <p>Email: <a href="mailto:info@aymicookie.com" class="text-white">info@aymicookie.com</a></p>
                 <p>Teléfono: <a href="tel:+34912345678" class="text-white">+34 912 345 678</a></p>
                 <p>Dirección: Calle Dulce, 123, Sevilla, España</p>
+
+                <!-- Mapa debajo de Contacto -->
+                <h5 class="text-uppercase mt-3">Ubicación</h5>
+                <iframe 
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3171.3342829084286!2d-5.981765187270792!3d37.35826643592485!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd126e7e6c13dfe7%3A0x854c5e174c910940!2sC.%20Dulce%20Chac%C3%B3n%2C%2041013%20Sevilla!5e0!3m2!1ses!2ses!4v1740655987128!5m2!1ses!2ses" 
+                    width="100%" 
+                    height="200" 
+                    style="border:0; border-radius: 10px;" 
+                    allowfullscreen="" 
+                    loading="lazy" 
+                    referrerpolicy="no-referrer-when-downgrade">
+                </iframe>
             </div>
 
             <!-- Sección de redes sociales -->
@@ -34,7 +46,7 @@
 
         <!-- Derechos reservados -->
         <div class="text-center">
-            <p class="mb-0">&copy; 2025 Ay Mi Cookie - Todos los derechos reservados.</p>
+            <p class="mb-0 fw-bold">&copy; 2025 Ay Mi Cookie - Todos los derechos reservados.</p>
         </div>
     </div>
 </footer>
